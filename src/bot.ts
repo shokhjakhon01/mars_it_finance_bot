fa92edcfc78f4c8d39b130e761b861934145ad53
shokhjakhon01 <78145953+shokhjakhon01@users.noreply.github.com>
2024-01-24 14:48:57 +0500

    without webhook

--- src/bot.ts
+++ src/bot.ts
@@ -16,24 +16,24 @@ const webHookPath = `/${process.env.BOT_API_TOKEN}/`;
 const webHookUrl = `${process.env.DOMAIN_NAME}${webHookPath}`;
 
 // Function to delete the webhook with error handling
-const deleteWebhook = async () => {
-  try {
-    await bot.api.deleteWebhook();
-    console.log("Webhook deleted successfully");
-  } catch (error) {
-    console.error("Error deleting webhook:", error);
-  }
-};
+// const deleteWebhook = async () => {
+//   try {
+//     await bot.api.deleteWebhook();
+//     console.log("Webhook deleted successfully");
+//   } catch (error) {
+//     console.error("Error deleting webhook:", error);
+//   }
+// };
 
 // Function to set the webhook with error handling
-const setWebhook = async () => {
-  try {
-    await bot.api.setWebhook(webHookUrl);
-    console.log("Webhook set successfully");
-  } catch (error) {
-    console.error("Error setting webhook:", error);
-  }
-};
+// const setWebhook = async () => {
+//   try {
+//     await bot.api.setWebhook(webHookUrl);
+//     console.log("Webhook set successfully");
+//   } catch (error) {
+//     console.error("Error setting webhook:", error);
+//   }
+// };
 
 // Set up session and connect to MongoDB
 bot.use(session());
@@ -44,25 +44,25 @@ commands(bot);
 controllers(bot);
 
 // Delete the existing webhook before starting the bot
-deleteWebhook();
+// deleteWebhook();
 
 // Start the bot
 bot.start();
 
 // Handle incoming updates via HTTP POST
-app.post(webHookPath, async (req, res) => {
-  try {
-    await bot.handleUpdate(req.body);
-    res.sendStatus(200);
-  } catch (error) {
-    console.error("Error handling update:", error);
-    res.sendStatus(500);
-  }
-});
+// app.post(webHookPath, async (req, res) => {
+//   try {
+//     await bot.handleUpdate(req.body);
+//     res.sendStatus(200);
+//   } catch (error) {
+//     console.error("Error handling update:", error);
+//     res.sendStatus(500);
+//   }
+// });
 
 // Set the new webhook and start the Express server
-setWebhook();
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// setWebhook();
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
